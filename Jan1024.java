1077611026ef34053edcab97a96f772046371bf1
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-01-10 12:09:40 +0530

    Jan/10/24

---
+++ Jan1024.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double M=sc.nextDouble();
+		double N=sc.nextDouble();
+		int X=sc.nextInt();
+		String format="%."+X+"f";
+		System.out.print(String.format(format,M/N));
+		sc.close();
+	}
+}
